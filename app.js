218e4d3485921c7250d3f5e01d29e2dbe1eb2633
Joseph Grigoletti <jgrig2@gmail.com>
2020-04-07 13:30:06 -0500

    set up basic app.js file

---
+++ app.js
@@ -0,0 +1,23 @@
+//requirements 
+
+//const express = require('express')
+//const server = express()
+
+
+//db
+
+
+
+//middlewares
+
+
+//7 restful routes 
+
+
+
+//controllers
+
+server.listen(3000, () => {
+
+    console.log("Server listening on 3000"); 
+})

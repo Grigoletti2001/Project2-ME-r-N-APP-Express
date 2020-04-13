950f81e37cacc609a339b5e389dc23768c84cf58
Joseph Grigoletti <jgrig2@gmail.com>
2020-04-13 14:06:26 -0500

    test deployment

--- models/journal.js
+++ models/journal.js
@@ -1,58 +1,70 @@
 const mongoose = require("mongoose");
 
 const journalSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: false
+  },
 
+  date: {
+    type: Date
+  },
 
-        title: {
-            type: String,
-            required: false
-        },
-        
-        date: {
-            type: Date,
-      
-         
-        }, 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false // dogs must have a user
+  },
+  intro: {
+    type: String,
+    required: false
+  },
+  emoticonIntro: {
+    type: String,
+    required: false
+  },
 
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: false // dogs must have a user
-        },
-        intro: {
-            type: String, 
-            required: false
-        }, 
-        context: {
-            type: String,
-            required: false
-        }, 
-        climax: {
-            type: String,
-            required: false
-        }, 
-        acceptance: {
-            type: String,
-            required: false
-        }, 
-        conclusion: {
-            type: String,
-            required: false
-        }, 
-        emotion: {
-            type: String, 
-            required: false
-        }
+  context: {
+    type: String,
+    required: false
+  },
+  emoticonContext: {
+    type: String,
+    required: false
+  },
 
+  climax: {
+    type: String,
+    required: false
+  },
 
-    });
-    
+  emoticonClimax: {
+    type: String,
+    required: false
+  },
 
+  acceptance: {
+    type: String,
+    required: false
+  },
 
+  emoticonAcceptance: {
+    type: String,
+    required: false
+  },
 
+  conclusion: {
+    type: String,
+    required: false
+  },
+  emoticonConclusion: {
+    type: String,
+    required: false
+  }
+});
 
 ///conenct journal to user (see journal to user... example. )
-  
-const Journal = mongoose.model ("Journal", journalSchema);
 
-module.exports = Journal; 
+const Journal = mongoose.model("Journal", journalSchema);
+
+module.exports = Journal;
